9522c44ab6b271631ba80b9622b09ef2a19a3fd7
Francisco Arredondo <frarredo@gmail.com>
2019-06-16 17:18:32 -0700

    http requests and updating state
    1. moved the users to the app level component state
    2. retrieve user data via HTTP GET request to github api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'octocat',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/583231?v=4',
-        html_url: 'https://github.com/octocat',
-      },
-      {
-        id: '2',
-        login: 'frarredo',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/583231?v=4',
-        html_url: 'https://github.com/octocat',
-      },
-      {
-        id: '3',
-        login: 'jumbo',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/583231?v=4',
-        html_url: 'https://github.com/octocat',
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
