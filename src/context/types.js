7f554e886f08325e71f8a5e7514da8913e7a7d1a
Francisco Arredondo <frarredo@gmail.com>
2019-06-29 10:45:58 -0700

    fixed a typo in types.js

--- src/context/types.js
+++ src/context/types.js
@@ -4,4 +4,4 @@ export const CLEAR_USERS = 'CLEAR_USERS';
 export const GET_REPOS = 'GET_REPOS';
 export const SET_LOADING = 'SET_LOADING';
 export const SET_ALERT = 'SET_ALERT';
-export const REMOVER_ALERT = 'REMOVER_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
